d5629ea0c1872f484ce41ebf186b69c7734cf9d4
hungtooc <hung59349214@gmail.com>
2021-10-16 09:53:28 +0700

    update export pdf to images

--- tools/pdf-to-images.py
+++ tools/pdf-to-images.py
@@ -7,25 +7,43 @@ import io
 import os
 from pathlib import Path
 from pdf2image import convert_from_path, convert_from_bytes, pdfinfo_from_bytes
+import argparse
+import sys
+from tqdm import tqdm
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pdf-dir', type=str, default=ROOT / 'data/input', help='dir to pdf files')
+    parser.add_argument('--output-dir', type=str, default=ROOT / 'data/raw-image/TỪ 13.10.20 ĐẾN 23.11.20', help='dir to save images')
+    parser.add_argument('--pdf-password', type=str, default=None, help='pdf password')
+    parser.add_argument('--from-page-no', type=int, default=1, help='extra image from page')
+    parser.add_argument('--to-page-no', type=int, default=-1, help='extra image to page')
+    parser.add_argument('--fix-page-number', type=int, default=0, help='fix page number (page_no += fix_page_number)')
+    opt = parser.parse_args()
+    return opt
+
 
 if __name__ == '__main__':
-
-    pdf_dir = "/home/hungtooc/Documents/SAOKE/Thuy-Tien/input/TÀI KHOẢN XXX746 (Pass_ Vcbsaoke@2021)/TỪ 13.10.20 ĐẾN 23.11.20"
-    output_dir = "../data/data-raw/TỪ 13.10.20 ĐẾN 23.11.20"
-    pdf_password = "Vcbsaoke@2021"
-    cv2.namedWindow("page", cv2.WINDOW_NORMAL)
+    opt = parse_opt()
+    pdf_dir = opt.pdf_dir
+    output_dir = opt.output_dir
+    pdf_password = opt.pdf_password
+    from_page_no = opt.from_page_no
+    to_page_no = opt.to_page_no
+    add_page_no = opt.fix_page_number
     for index, pdf_path in enumerate(sorted(glob.glob(f"{pdf_dir}/*.pdf"))[:1]):
-        print(f"(None) processing {pdf_path}...")
         sub_output_dir = f"{output_dir}/{ntpath.basename(pdf_path)}/"
         Path(sub_output_dir).mkdir(parents=True, exist_ok=True)
         info = pdfinfo_from_bytes(open(pdf_path, "rb").read(), userpw=pdf_password, poppler_path=None)
-        # to_page_no = 18107
-        from_page_no = 1
-        # to_page_no = info['Pages']  # max page no
-        to_page_no = 10
-        add_page_no = 0
-        print(f"maxPages: {to_page_no}")
-        for page in range(from_page_no, to_page_no + 1, 50):
+        to_page_no = to_page_no if to_page_no != -1 else info['Pages']
+        print(f"processing {ntpath.basename(pdf_path)} from page {from_page_no} to page {to_page_no}, with appending page number {add_page_no}")
+        for page in tqdm(range(from_page_no, to_page_no + 1, 50)):
             images = convert_from_bytes(open(pdf_path, "rb").read(), first_page=page, last_page=min(page + 49, to_page_no), userpw=pdf_password)
             print(f"from page {page} to {min(page + 49, to_page_no)}, len = {len(images)}")
             for index, image in enumerate(images):
